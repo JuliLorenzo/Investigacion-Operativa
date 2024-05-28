70cc69d4312ccb88404fa32efe218c27682dab7d
Camila <camilacitro1106@gmail.com>
2024-05-28 20:17:03 -0300

    Inicializando el proyecto

---
+++ src/main/java/invop/InvOpApplication.java
@@ -0,0 +1,13 @@
+package invop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvOpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvOpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/invop/InvOpApplicationTests.java
@@ -0,0 +1,13 @@
+package invop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvOpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
