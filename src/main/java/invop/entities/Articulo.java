3ec737057a529ccd5835ff324e987620b9358d24
luciaburky <luciaburky1@gmail.com>
2024-05-29 18:19:20 -0300

    agregué bases

--- src/main/java/invop/entities/Articulo.java
+++ src/main/java/invop/entities/Articulo.java
@@ -1,8 +1,10 @@
 package invop.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.*;
+import org.antlr.v4.runtime.misc.NotNull;
 
 @Entity
 @Table(name = "articulo")
@@ -12,5 +14,29 @@ import lombok.*;
 @Setter
 @Builder
 public class Articulo extends Base {
+
+    @NotNull
+    @Column(name = "nombre_articulo")
+    private String nombre_articulo;
+
+    @NotNull
+    @Column(name = "precio_articulo")
+    private Double precio_articulo;
+
+    @NotNull
+    @Column(name = "cantidad_articulo")
+    private int cantidad_articulo;
+
+    @Column(name = "lote_optimo_articulo")
+    private int lote_optimo_articulo;
+
+    @Column(name = "lote_optimo_articulo")
+    private int punto_pedido_articulo;
+
+    @Column(name = "lote_optimo_articulo")
+    private int stock_seguridad_articulo;
+
+    @Column(name = "cgi_articulo")
+    private Double cgi_articulo;
 
 }
---
+++ src/main/java/invop/entities/Proveedor.java
@@ -0,0 +1,21 @@
+package invop.entities;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.antlr.v4.runtime.misc.NotNull;
+
+@Entity
+@Table(name = "articulo")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Proveedor extends Base {
+    @NotNull
+    @Column(name = "nombre_proveedor")
+    private String nombreProveedor;
+
+
+}
