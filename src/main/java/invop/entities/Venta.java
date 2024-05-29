86bed2eb3b61ab2c50f4e15bb526407da35775e3
maximocostaroman <maximo.costa@alumnos.frm.utn.edu.ar>
2024-05-29 18:21:08 -0300

    Primer commit maxi

---
+++ src/main/java/invop/entities/Venta.java
@@ -0,0 +1,24 @@
+package invop.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "articulo_insumo")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Venta extends Base{
+
+    @NotNull
+    @Column(name = "fecha_venta")
+    private Date fecha_venta;
+
+}
