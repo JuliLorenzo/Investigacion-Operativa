1786b6e0d0ccd3916aa0cd942e8635f07af37204
maximocostaroman <maximo.costa@alumnos.frm.utn.edu.ar>
2024-05-31 11:31:55 -0300

    "Rel ventadetalle con articulo"

--- src/main/java/invop/entities/VentaDetalle.java
+++ src/main/java/invop/entities/VentaDetalle.java
@@ -1,8 +1,6 @@
 package invop.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 import org.antlr.v4.runtime.misc.NotNull;
 
@@ -22,4 +20,8 @@ public class VentaDetalle extends Base{
 
 
 
+    @NotNull
+    @ManyToOne()
+    @JoinColumn(name = "id_articulo")
+    private Articulo articulo;
 }
