5e464d8e0036b366cd3d8de251c2defc324b80f0
luciaburky <luciaburky1@gmail.com>
2024-06-01 19:04:14 -0300

    cambio de nombre, de DemandaHistoricaImpl a DemandaHistoricaServiceImpl

--- src/main/java/invop/services/DemandaHistoricaServiceImpl.java
+++ src/main/java/invop/services/DemandaHistoricaServiceImpl.java
@@ -8,11 +8,13 @@ import org.springframework.stereotype.Service;
 import java.util.Date;
 
 @Service
-public class DemandaHistoricaImpl extends BaseServiceImpl<DemandaHistorica, Long> implements DemandaHistoricaService {
+public class DemandaHistoricaServiceImpl extends BaseServiceImpl<DemandaHistorica, Long> implements DemandaHistoricaService {
 
     @Autowired
-    public DemandaHistoricaImpl(DemandaHistoricaRepository demandaHistoricaRepository){
+    private DemandaHistoricaRepository demandaHistoricaRepository;
+    public DemandaHistoricaServiceImpl(DemandaHistoricaRepository demandaHistoricaRepository){
         super(demandaHistoricaRepository);
+        this.demandaHistoricaRepository = demandaHistoricaRepository;
     }
 
     public void calcularDemandaHistorica(Date fechaDesde, Date fechaHasta, Long idArticulo){
