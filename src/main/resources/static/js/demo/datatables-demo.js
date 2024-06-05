b757755069a76376972b29151017f82f5b1ba30a
maximocostaroman <maximo.costa@alumnos.frm.utn.edu.ar>
2024-06-05 18:17:05 -0300

    Mas cosas front

--- src/main/resources/static/js/demo/datatables-demo.js
+++ src/main/resources/static/js/demo/datatables-demo.js
@@ -26,3 +26,23 @@ document.addEventListener("DOMContentLoaded", function() {
         console.error("Error al obtener los artículos:", error);
       });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  fetch("http://localhost:9090/api/v1/proveedores")
+      .then(response => response.json())
+      .then(data => {
+        const tableBody = document.querySelector("#proveedores-table tbody");
+        data.forEach(proveedor => {
+          const row = document.createElement("tr");
+          row.innerHTML = `
+                    <td>${proveedor.id}</td>
+                    <td>${proveedor.nombreProveedor}</td>
+                    
+                `;
+          tableBody.appendChild(row);
+        });
+      })
+      .catch(error => {
+        console.error("Error al obtener los proveedores:", error);
+      });
+});
