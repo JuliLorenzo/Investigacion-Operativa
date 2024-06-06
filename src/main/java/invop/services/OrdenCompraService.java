aa626c1cf0568aecfafb5b273b91acd15e8bca95
Camila <camilacitro1106@gmail.com>
2024-06-05 23:29:11 -0300

    Borre los q estaban de mas

--- src/main/java/invop/services/OrdenCompraService.java
+++ src/main/java/invop/services/OrdenCompraService.java
@@ -7,6 +7,5 @@ import java.util.List;
 public interface OrdenCompraService extends BaseService<OrdenCompra, Long> {
     // Métodos específicos de Orden de Compra
     public List<OrdenCompra> findOrdenCompraByEstado(String filtroEstado) throws Exception;
-    public boolean controlEstadoOrdenes() throws Exception;
     public boolean articuloConOrdenActiva(Long articuloId) throws Exception;
 }
