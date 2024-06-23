3eeed72b0448453881c38817329ce5442d6b875a
luciaburky <luciaburky1@gmail.com>
2024-06-23 12:47:35 -0300

    cambios en tabla detalles OC

--- src/main/resources/static/js/demo/datatable-OrdenDeCompraDetalle.js
+++ src/main/resources/static/js/demo/datatable-OrdenDeCompraDetalle.js
@@ -3,40 +3,21 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(response => response.json())
         .then(data => {
             const tableBody = document.querySelector("#ordenesdecompradetalle-table tbody");
-
-            // Iterar sobre cada detalle de orden de compra
-            data.forEach(ordenesdecomprasdetalles => {
-                const row = document.createElement("tr");
-
-                // Hacer una solicitud para obtener el nombre del artículo
-                fetch(`http://localhost:9090/api/v1/articulos/${ordenesdecomprasdetalles.articulo.id}`)
-                    .then(response => response.json())
-                    .then(articulo => {
-                        // Agregar los datos al row de la tabla
-                        row.innerHTML = `
-                            <td>${ordenesdecomprasdetalles.id}</td>
-                            <td>${articulo.nombreArticulo}</td>
-                            <td>${ordenesdecomprasdetalles.cantidadAComprar}</td>
-                            <td>${ordenesdecomprasdetalles.ordenCompra ? ordenesdecomprasdetalles.ordenCompra.id : 'No asignado'}</td>
-                            <td>
-                                <div style="text-align: center">
-                                    <a href="#" class="btn btn-info btn-circle btn-sm" data-id="${ordenesdecomprasdetalles.id}">
-                                        <i class="fas fa-link"></i>
-                                    </a>
-                                    <a href="#" class="btn btn-warning btn-circle btn-sm" data-id="${ordenesdecomprasdetalles.id}">
-                                        <i class="fas fa-edit"></i>
-                                    </a>
-                                </div>
-                            </td>
-                        `;
-                        tableBody.appendChild(row);
-                    })
-                    .catch(error => {
-                        console.error("Error al obtener el artículo:", error);
-                    });
-            });
-        })
+            data.forEach(ordencompra => {
+                ordencompra.ordenCompraDetalles.forEach(detalle => {
+                    const row = document.createElement("tr");
+                    row.innerHTML = `
+                    <td>${detalle.id}</td>
+                    <td>${ordencompra.id}</td>
+                    <td>${ordencompra.fechaOrdenCompra}</td>
+                    <td>${detalle.articulo.nombreArticulo}</td>
+                    <td>${detalle.cantidadAComprar}</td>
+                    `;
+                    tableBody.appendChild(row);
+                });
+            })
         .catch(error => {
             console.error("Error al obtener los detalles de las órdenes de compra:", error);
         });
 });
+})
