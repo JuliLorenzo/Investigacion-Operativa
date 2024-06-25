ea350e049b5fb494941838c73eb1dda135e25ae7
luciaburky <luciaburky1@gmail.com>
2024-06-25 19:04:26 -0300

    fin tabla predicciondemanda

--- src/main/resources/static/js/demo/datatable-PrediccionDemanda.js
+++ src/main/resources/static/js/demo/datatable-PrediccionDemanda.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
+    let metodoPredeterminado = null;
+
     $('#crearPrediccionModal').on('show.bs.modal', function () {
         cargarArticulos();
         cargarMeses();
@@ -32,13 +34,15 @@ document.addEventListener("DOMContentLoaded", function () {
     function cargarMetodo(articuloSelect) {
         articuloSelect.addEventListener("change", function () {
             const selectedArticuloId = articuloSelect.value;
+
             if (selectedArticuloId) {
                 fetch(`http://localhost:9090/api/v1/articulos/${selectedArticuloId}`)
                     .then(response => response.json())
                     .then(data => {
+                        metodoPredeterminado = data.metodoPrediccionPredeterminado;
                         const metodoInput = document.querySelector("#metodo");
                         metodoInput.value = metodoMapping[data.metodoPrediccionPredeterminado] || 'Método Desconocido';
-                        mostrarCamposExtra(data.metodoPrediccionPredeterminado);
+                        mostrarCamposExtra(metodoPredeterminado);
                     })
                     .catch(error => {
                         console.error('Error al obtener el método de predicción predeterminado del artículo:', error);
@@ -124,25 +128,120 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    window.submitForm = function () {
+    /*function submitForm() {
         const form = document.querySelector("#crearPrediccionForm");
         const formData = new FormData(form);
         const datosPrediccion = {
             idArticulo: formData.get("articulo"),
-            cantidadPeriodosAdelante: formData.get("cantidadPeriodosAdelante"),
-            mesAPredecir: formData.get("mesAPredecir"),
-            anioAPredecir: formData.get("anioAPredecir"),
+            cantidadPeriodosAdelante: parseInt(formData.get("cantidadPeriodosAdelante")),
+            mesAPredecir: parseInt(formData.get("mesAPredecir")),
+            anioAPredecir: parseInt(formData.get("anioAPredecir")),
             nombreMetodoPrediccion: formData.get("metodo"),
-            cantidadDemandaAnualTotal: formData.get("cantidadDemandaAnualTotal") || null,
-            cantidadPeriodosHistoricos: formData.get("cantidadPeriodosHistoricos") || null,
+            cantidadDemandaAnualTotal: formData.get("cantidadDemandaAnualTotal") ? parseInt(formData.get("cantidadDemandaAnualTotal")) : null,
+            cantidadPeriodosHistoricos: formData.get("cantidadPeriodosHistoricos") ? parseInt(formData.get("cantidadPeriodosHistoricos")) : null,
             coeficientesPonderacion: Array.from(document.querySelectorAll('[name="coeficientesPonderacion"]')).map(input => parseFloat(input.value)) || null,
-            alfa: formData.get("alfa") || null
+            alfa: formData.get("alfa") ? parseFloat(formData.get("alfa")) : null
         };
-
-        // Add other fields if necessary
         console.log(datosPrediccion);
-        // You can now send datosPrediccion to your backend
-    }
+        fetch("http://localhost:9090/api/v1/prediccionesdemandas/crearPredicciones", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(datosPrediccion)
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok ' + response.statusText);
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log("Predicción creada");
+                console.log()
+            })
+            .catch(error => {
+                console.error("Error al crear predicciones:", error);
+            });
+    }*/
+
+    //document.getElementById('guardarPrediccion').addEventListener('click', submitForm);
+    document.querySelector("#guardarPrediccion").addEventListener("click",
+        function () {
+            const form = document.querySelector("#crearPrediccionForm");
+            let periodosHistoricos = null;
+            let alfa = null;
+            let cantidadDemandaAnualTotal = null;
+            let arrayCoefValue = [];
+            switch (metodoPredeterminado) {
+                case ("ESTACIONALIDAD"): {
+                    cantidadDemandaAnualTotal = document.querySelector('#cantidadDemandaAnualTotal').value;
+                    break
+                }
+                case ("PROMEDIO_MOVIL_PONDERADO"): {
+                    periodosHistoricos = document.querySelector('#cantidadPeriodosHistoricos').value;
+                    for (let i = 0; i < periodosHistoricos; i++) {
+                        const coefValue = parseFloat(document.querySelector(`#coeficientePonderacion${i + 1}`).value);
+                        arrayCoefValue.push(coefValue);
+                    }
+                    break
+                }
+                case ("REGRESION_LINEAL"): {
+                    periodosHistoricos = document.querySelector('#cantidadPeriodosHistoricos').value;
+                    break
+                }
+                case ("PROMEDIO_MOVIL_SUAVIZADO"): {
+                    alfa = parseFloat(document.querySelector('#alfa').value);
+                    break
+                }
+            }
+            const formData = {
+                cantidadPeriodosHistoricos: periodosHistoricos,
+                cantidadPeriodosAdelante: document.querySelector('input[name="cantidadPeriodosAdelante"]:checked').value,
+                coeficientesPonderacion: arrayCoefValue,
+                idArticulo: document.querySelector('#articulo').value,
+                mesAPredecir: document.querySelector('#mesAPredecir').value,
+                anioAPredecir: document.querySelector('#anioAPredecir').value,
+                alfa: alfa,
+                nombreMetodoPrediccion: metodoPredeterminado,
+                cantidadDemandaAnualTotal: cantidadDemandaAnualTotal,
+                fechaDesde: null,
+                fechaHasta: null,
+            };
+            console.log("Enviando datos... :", formData);
+            fetch("http://localhost:9090/api/v1/prediccionesdemandas/crearPredicciones", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(formData)
+            })
+                .then(response => {
+                    $('#crearPrediccionModal').modal('hide');
+                    alert("¡Predicción creada!");
+                    location.reload();
+                    const tableBody = $('#predicciones-table tbody');
+                    const row = document.createElement("tr");
+                    const fechaPred = new Date(response.fechaPrediccion + "T00:00:00");
+                    const mes = (fechaPred.getMonth()) + 1;
+                    const anio = fechaPred.getFullYear();
+                    const metodoUsado = metodoMapping[response.nombreMetodoUsado] || 'Método Desconocido';
+                    row.innerHTML = `
+                        <td>${response.id}</td>
+                        <td>${mes}</td>
+                        <td>${anio}</td>
+                        <td>${response.valorPrediccion}</td>
+                        <td>${response.articulo.nombreArticulo}</td>
+                        <td>${metodoUsado}</td>
+                    `;
+                    tableBody.appendChild(row);
+                    $('#crearPrediccionForm')[0].reset();
+                    //return response.json();
+                })
+                .catch(error => {
+                    console.error("Error al crear predicciones:", error);
+                })
+        })
 
     // Fetch artículos para filtrar
     function cargarArticulosFiltro() {
