3904caf2694f1ba08c9308ad83141997cbc65a4b
luciaburky <luciaburky1@gmail.com>
2024-06-26 21:39:12 -0300

    cambio tipo de dato de cantidadVendida a Integer

--- src/main/java/invop/repositories/DemandaHistoricaRepository.java
+++ src/main/java/invop/repositories/DemandaHistoricaRepository.java
@@ -2,8 +2,22 @@ package invop.repositories;
 
 
 import invop.entities.DemandaHistorica;
+import invop.entities.Venta;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface DemandaHistoricaRepository extends BaseRepository<DemandaHistorica, Long> {
+    @Query(
+            value = "SELECT * " +
+                    "FROM demandas_historicas " +
+                    "WHERE id_articulo = :idArticulo",
+            nativeQuery = true
+    )
+    List<DemandaHistorica> buscarDemandasHistoricasPorArticulo(@Param ("idArticulo") Long idArticulo);
+
 }
